6b0df4ff2f9828baab17441ea38d0194c8932d50
Vũ Đức Đạt <vha9222@gmail.com>
2026-01-31 10:56:29 +0700

    Update Project1.bat

--- Project1.bat
+++ Project1.bat
@@ -1,49 +1,62 @@
 @echo off
+title Tao Va Chay Web ASP.NET Core
 
 
-
-:: --- 1. KIEM TRA .NET ---
+:: --- 1. KIEM TRA MOI TRUONG ---
 dotnet --version >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [LOI] May tinh chua cai .NET SDK!
+    color 0C
+    echo [LOI] May tinh chua cai .NET SDK.
+    echo Vui long cai dat tai dotnet.microsoft.com
     pause
     exit
 )
 
+echo ==================================================
+echo   TAO DU AN ASP.NET CORE MVC + AUTO RUN
+echo ==================================================
 
-:: --- 2. NHAP TEN ---
-set /p projectName="Nhap ten du an (VD: MyWeb): "
+:: --- 2. NHAP TEN DU AN ---
+set /p projectName="Nhap ten du an (VD: MyShop): "
 if "%projectName%"=="" goto Error
 
 :: --- 3. TAO PROJECT ---
 echo.
-echo Dang tao project "%projectName%"...
+echo [1/4] Dang tao project "%projectName%"...
 dotnet new mvc -n %projectName%
+if %errorlevel% neq 0 goto Error
 
-if %errorlevel% neq 0 (
-    echo [LOI] Ten du an khong hop le.
-    pause
-    exit
-)
-
-:: --- 4. MO DU AN (OPEN LUON) ---
 cd %projectName%
 
+:: --- 4. CAI DAT CHUNG CHI HTTPS (Quan trong) ---
+:: Buoc nay giup trinh duyet khong bao loi "Bao mat" mau do
 echo.
-echo ==================================================
-echo DA XONG! DANG MO PROJECT...
-echo ==================================================
+echo [2/4] Dang cai dat chung chi HTTPS...
+dotnet dev-certs https --trust
 
-:: 1. Mo thu muc chua file (File Explorer)
+:: --- 5. MO CONG CU LAP TRINH ---
+echo.
 start .
-
-
-
-:: 3. (Tuy chon) Neu ban dung Visual Studio 2022 (Icon Tim), no se mo file .csproj
 :: start "" "%projectName%.csproj"
 
+:: --- 6. CHAY DU AN ---
+echo.
+echo [4/4] DANG KHOI DONG SERVER WEB...
+echo ==================================================
+echo   LUU Y QUAN TRONG:
+echo   1. Cho mot lat de web tai thu vien (Restore).
+echo   2. Web se tu dong bat len trinh duyet.
+echo   3. De dung web, hay nhan to hop phim: Ctrl + C
+echo ==================================================
+echo.
+
+:: Lenh nay se chay web va theo doi chinh sua (Hot Reload)
+dotnet watch run
+
+pause
 exit
 
 :Error
-echo Ban chua nhap ten!
+echo.
+echo [LOI] Co loi xay ra hoac ban chua nhap ten.
 pause
